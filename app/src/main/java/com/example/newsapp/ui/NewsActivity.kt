47f5b8a06559f187b5d084da61af42160cd2831d
Mikhail Tabunov <mtt@d-element.ru>
2023-09-25 12:11:58 +0500

    Базовая настройка нижнего меню

---
+++ app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
@@ -0,0 +1,30 @@
+package com.example.newsapp.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.newsapp.R
+import com.example.newsapp.databinding.ActivityNewsBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class NewsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityNewsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNewsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navView: BottomNavigationView = binding.bottomNavigationView
+
+        val navController = findNavController(R.id.newsNavHostFragment)
+
+        navView.setupWithNavController(navController)
+
+    }
+
+
+}
