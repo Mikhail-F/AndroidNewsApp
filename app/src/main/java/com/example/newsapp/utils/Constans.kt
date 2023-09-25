2b1a0b07ba665c7a9f0ce882cd30f7b60a8b2a5c
Mikhail Tabunov <mtt@d-element.ru>
2023-09-25 12:49:31 +0500

    Базовая настройка апи

---
+++ app/src/main/java/com/example/newsapp/utils/Constans.kt
@@ -0,0 +1,8 @@
+package com.example.newsapp.utils
+
+class Constans {
+    companion object {
+        const val API_KEY = "29ff8b04edf64f06add1e1625b0f5fe2"
+        const val BASE_URL = "https://newsapi.org/"
+    }
+}
