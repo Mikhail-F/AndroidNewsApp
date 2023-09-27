18e429eb726f0bb8ebd34b8082d171e6161d932d
Mikhail Tabunov <mtt@d-element.ru>
2023-09-27 11:55:40 +0500

    Подготовил провайдеры и обработку ошибок

--- app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/example/newsapp/ui/NewsActivity.kt
@@ -2,29 +2,36 @@ package com.example.newsapp.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.newsapp.R
 import com.example.newsapp.databinding.ActivityNewsBinding
+import com.example.newsapp.db.ArticleDatabase
+import com.example.newsapp.repository.NewsRepository
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class NewsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityNewsBinding
+    lateinit var viewModel: NewsViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNewsBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val navView: BottomNavigationView = binding.bottomNavigationView
-
-        val navController = findNavController(R.id.newsNavHostFragment)
-
-        navView.setupWithNavController(navController)
-
+        initProviders()
+        initBottomMenu()
     }
 
+    private fun initProviders() {
+        val newsRepository = NewsRepository(ArticleDatabase(this))
+        val viewModelProviderFactory = NewsNewModelProviderFactory(newsRepository)
+        viewModel = ViewModelProvider(this, viewModelProviderFactory).get(NewsViewModel::class.java)
+    }
 
+    private fun initBottomMenu() {
+        val navView: BottomNavigationView = binding.bottomNavigationView
+        val navController = findNavController(R.id.newsNavHostFragment)
+        navView.setupWithNavController(navController)
+    }
 }
