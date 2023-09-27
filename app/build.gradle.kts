35eda42068b394bbd4d625d4c981a08f20d49309
Mikhail Tabunov <mtt@d-element.ru>
2023-09-28 00:06:54 +0500

    Добавил ksp и повысил gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -37,7 +38,7 @@ android {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.9.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
@@ -50,6 +51,7 @@ dependencies {
 
     // Room
     implementation ("androidx.room:room-runtime:2.5.2")
+    ksp ("androidx.room:room-compiler:2.5.2")
 
     // Kotlin Extensions and Coroutines support for Room
     implementation ("androidx.room:room-ktx:2.5.2")
@@ -73,4 +75,5 @@ dependencies {
 
     // Glide
     implementation ("com.github.bumptech.glide:glide:4.14.2")
+    ksp ("com.github.bumptech.glide:compiler:4.14.2")
 }
